7ddf5ced2bccf50240149fe4531380f7c7af9509
Michael Bosworth <mtbosworth@gmail.com>
2013-05-29 19:22:21 -0500

    hbs setup!

---
+++ app.js
@@ -0,0 +1,45 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , hbs = require('express-hbs');
+
+hbs.express3({
+  extname: ".html",
+  layout: "layout"
+});
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.engine('html', hbs.express3({
+    partialsDir: __dirname + '/views',
+    defaultLayout: __dirname + '/views/layout.html'
+  }));
+  app.set('view engine', 'html');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
